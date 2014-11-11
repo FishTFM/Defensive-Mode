6959bdc08c932e3e7fef4ed2ab28214d3f11ffa3
Fish <skyzoh.key@gmail.com>
2014-11-11 14:43:15 +0100

    Update defensive.lua

--- defensive.lua
+++ defensive.lua
@@ -81,38 +81,3 @@ Defensers = {
 
 FFA = {
 }
-
-
--- TODO:
----- Scoreboard ;
----- FFA System ;
----- Profilboard ;
----- Defensers.
-
--- Events.
-function eventChatCommand(name, command)
-	--local args = string.gmatch(command, "[^%s]+");
-	--print(unpack(args));
-end;
-
-function eventNewPlayer(name)
-	Players:init(name);
-end;
-
-function eventPlayerDied(name)
-	Players:incrementStat(name, "death", 1);
-end;
-
-function eventPlayerGetCheese(name)
-	Players:incrementStat(name, "cheese", 1);
-end;
-
-function eventPlayerWon(name)
-	Scoreboard:playerEnter(name);
-	Players:incrementStat(name, "hole", 1);
-
-	-- TODO: Check firsts.
-	if Scoreboard:getPlace(name) < 4 then
-		Players:incrementStat(name, "points", 4 - Scoreboard:getPlace(name));
-	end
-end;
